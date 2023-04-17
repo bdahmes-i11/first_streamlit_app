098979ca6e0a0751a83ca69234b337cfad18209c
bdahmes-i11 <130000452+bdahmes-i11@users.noreply.github.com>
2023-04-17 09:24:50 -0500

    Update streamlit_app.py
    introduced function call

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{this_fruit_choice}")
+  return pd.json_normalize(fruityvice_response.json())
+
 streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
@@ -25,14 +29,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
-
-# Normalization/formatting 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# Dataframe to app
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    streamlit.dataframe(get_fruityvice_data(fruit_choice))
+    
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
